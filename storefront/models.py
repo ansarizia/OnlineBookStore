cd553ec2bb8728c31886e51fbd8cfc2939e4f21e
ansarizia <ansariziatec@gmail.com>
2017-02-04 14:09:12 +0530

    Added fields for books in models

--- storefront/models.py
+++ storefront/models.py
@@ -3,13 +3,16 @@
 from django.db import models
 
 class Books(models.Model):
-    Book_Name = models.CharField(max_length=200)
-    Author = models.CharField(max_length=200)
-    Book_logo = models.CharField(max_length=250)
-    Genre = models.CharField(max_length=300)
+    book_Name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    book_logo = models.CharField(max_length=250)
+    book_genre = models.CharField(max_length=300)
+    book_desc = models.CharField(max_length=400)
+    publish_date = models.DateField()
+
 
     def __str__(self):
-        return self.Book_Name + '-' + self.Author
+        return self.book_Name + '-' + self.author
 
 
 
