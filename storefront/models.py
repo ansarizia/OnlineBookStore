ab8d33574702bcd68e5953ad4eb8bb1152d2a292
Anendra.Gupta <gupta.anendra@gmail.com>
2017-02-04 12:10:16 +0530

    "Added one new model"

--- storefront/models.py
+++ storefront/models.py
@@ -1,5 +1,18 @@
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    Book_Name = models.CharField(max_length=200)
+    Author = models.CharField(max_length=200)
+    Book_logo = models.CharField(max_length=250)
+    Genre = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.Book_Name + '-' + self.Author
+
+
+
+
+
+
