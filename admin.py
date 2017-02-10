ceb813f8ed07d26ee3feef512764ed843c2e3845
anendragupta <gupta.anendra@gmail.com>
2017-02-10 12:56:04 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Books
-from .models import Languages,Genres
-
-admin.site.register(Books)
-admin.site.register(Languages)
-admin.site.register(Genres)
